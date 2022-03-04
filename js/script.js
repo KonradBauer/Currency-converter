e31e11d09ffa0565b8932301f3f7340281c12050
Konrad Bauer <konradbauer94@gmail.com>
2022-03-04 13:10:58 +0100

    added layout

---
+++ js/script.js
@@ -0,0 +1,25 @@
+let amountElement = document.querySelector(".js-amount");
+let currencyElement = document.querySelector(".js-currency");
+let resultElement = document.querySelector(".js-result");
+let formElement = document.querySelector(".js-form");
+
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let result = resultElement;
+    let currency = currencyElement.value;
+    let eurRate = 4.79;
+    let usdRate = 4.32;
+    let amount = +amountElement.value;
+
+    switch (currency) {
+        case "EUR":
+            result = amount / eurRate;
+            break;
+        case "USD":
+            result = amount / usdRate;
+            break;
+    };
+
+    resultElement.innerHTML = `Result: <strong>${result.toFixed(2)} ${currency}</strong>`;
+});
